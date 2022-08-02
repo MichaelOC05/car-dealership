da338f9f5c8322c92c278982da8f7a609de16a98
Michael O'Connell <mikematoconnell@gmail.com>
2022-08-01 20:27:47 -0400

    finished work on inventory forms

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AutomobileForm from './CreateAutomobile';
+import VehicleModelForm from './CreateVehicleModel';
+import ManufacturerForm from './CreateManufacturer';
+
+
 
 function App() {
   return (
@@ -9,6 +14,12 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="inventory/automobile/create/" element={<AutomobileForm />} />
+          <Route path="inventory/model/create/" element={<VehicleModelForm />} />
+          <Route path="inventory/manufacturer/create/" element={<ManufacturerForm />} />
+          {/* <Route path="" element={< />} />
+          <Route path="" element={< />} />
+          <Route path="" element={< />} /> */}
         </Routes>
       </div>
     </BrowserRouter>
---
+++ ghi/app/src/CreateManufacturer.js
@@ -0,0 +1,66 @@
+import React from 'react';
+
+class NewManufacturerForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChangeName = this.handleChangeName.bind(this);
+  }
+
+
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const data = {...this.state};
+    delete data.locations;
+    delete data.picture_url
+    console.log("thsi is data", data)
+    const hatsUrl = "http://localhost:8100/api/manufacturers/";
+    const fetchConfig = {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+    const response = await fetch(hatsUrl, fetchConfig);
+    if (response.ok) {
+      const newManufacturer = await response.json();
+      console.log(newManufacturer);
+      this.setState({
+        name: "",
+      });
+    }
+  }
+
+  handleChangeStyleName(event) {
+    const value = event.target.value;
+    this.setState({ name: value });
+  }
+
+
+  render() {
+    return (
+      <div className="row">
+        <div className="offset-3 col-6">
+          <div className="shadow p-4 mt-4">
+            <h1>Create a Manufacturer</h1>
+            <form onSubmit={this.handleSubmit} id="create-conference-form">
+              <div className="form-floating mb-3">
+                <input onChange={this.handleChangeName} placeholder="Name" required type="text" name="name" id="name" className="form-control" />
+                <label htmlFor="name">Name</label>
+              </div>
+              <button className="btn btn-primary">Create</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ManufacturerForm;
